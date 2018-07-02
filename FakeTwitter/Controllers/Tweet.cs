eae404a166e5dc5cf3f79770f48908e479d44e08
carlos.gg.eduardo <carlos.gg.eduardo@gmail.com>
2018-07-02 17:13:45 -0500

    FirstCommit

---
+++ FakeTwitter/Controllers/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeTwitter.Controllers
+{
+    public class Tweet
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        //                                                  //PROPERTIES
+        //-------------------------------------------------------------------------------------------------------------
+        public int Id { get; set; }
+        public string Likes { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Text { get; set; }
+        public string Images { get; set; }
+        //-------------------------------------------------------------------------------------------------------------
+        //                                                  //RELATIONSHIPS
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual User User { get; set; }
+        public virtual Tweet Response { get; set; }
+    }
+}
---
+++ FakeTwitter/Controllers/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeTwitter.Controllers
+{
+    public class User
+    {
+        //-------------------------------------------------------------------------------------------------------------
+        //                                                  //PROPERTIES
+        //-------------------------------------------------------------------------------------------------------------
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string At { get; set; }
+        public string Photo { get; set; }
+        //-------------------------------------------------------------------------------------------------------------
+        //                                                  //RELATIONSHIPS
+        //-------------------------------------------------------------------------------------------------------------
+        public virtual Group Groups { get; set; }
+        public virtual List<Tweet> Tweets { get; set; }
+    }
+}
